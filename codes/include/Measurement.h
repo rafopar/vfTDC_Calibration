9bf03eee8eeee10f76e061caaf76cf18c04a069b
Rafayel Paremuzyan <rafopar@rafopar-rhel8.jlab.org>
2022-05-12 17:29:36 -0400

    class for Multimeasurement is added, which is still work in progress, though is operational too

---
+++ codes/include/Measurement.h
@@ -0,0 +1,91 @@
+/* 
+ * File:   Measurement.h
+ * Author: rafopar
+ *
+ * Created on May 10, 2022, 3:58 PM
+ */
+
+#ifndef MEASUREMENT_H
+#define MEASUREMENT_H
+
+
+struct tdcHit {
+    int rawTDC;
+    int interval;
+    int tdcBin;
+};
+
+namespace vfTDCFuncs{
+    bool CompareInterval( tdcHit*, tdcHit* );
+}
+
+class Measurement {
+public:
+    Measurement(std::vector<tdcHit*>&);
+    Measurement(const Measurement& orig);
+    virtual ~Measurement();
+private:
+
+protected:
+
+    int fNmeasurements;
+    std::vector<tdcHit*> f_v_Hits;
+};
+
+//________________________________________________________________________
+
+class DoubleMeasure : public Measurement {
+public:
+    DoubleMeasure(std::vector<tdcHit*> & );
+    
+    int Get1stInterval();
+    int Get1stTDCBin();
+    int Get1stRawTDC();
+    int Get2ndInterval();
+    int Get2ndTDCBin();
+    int Get2ndRawTDC();
+    
+
+
+private:
+    int f1stInterval;
+    int f2ndInterval;
+    int f1stTDCBin;
+    int f2ndTDCBin;
+    int f1stRawTDC;
+    int f2ndRawTDC;
+
+
+    void AnalyzeMeasurement();
+};
+
+
+class TripleMeasure : public Measurement{
+
+    TripleMeasure( std::vector<tdcHit*> & );
+    
+    int Get1stInterval();
+    int Get1stTDCBin();
+    int Get1stRawTDC();
+    int Get2ndInterval();
+    int Get2ndTDCBin();
+    int Get2ndRawTDC();
+    int Get3rdInterval();
+    int Get3rdTDCBin();
+    int Get3rdRawTDC();
+    
+private:
+    int f1stInterval;
+    int f1stTDCBin;
+    int f1stRawTDC;
+    int f2ndInterval;
+    int f2ndTDCBin;
+    int f2ndRawTDC;
+    int f3rdInterval;
+    int f3rdTDCBin;
+    int f3rdRawTDC;
+    
+    void AnalyzeMeasurement();
+};
+
+#endif /* MEASUREMENT_H */
---
+++ codes/src/Measurement.cc
@@ -0,0 +1,106 @@
+/* 
+ * File:   Measurement.cc
+ * Author: rafopar
+ * 
+ * Created on May 10, 2022, 3:58 PM
+ */
+
+#include <vector>
+#include <iostream>
+#include <bits/stdc++.h>
+#include "Measurement.h"
+
+using namespace std;
+
+Measurement::Measurement(std::vector<tdcHit*> &v_Hits) {
+    f_v_Hits = v_Hits;
+    fNmeasurements = f_v_Hits.size();
+
+}
+
+Measurement::Measurement(const Measurement& orig) {
+}
+
+Measurement::~Measurement() {
+}
+
+//____________________________________________________________________________
+
+DoubleMeasure::DoubleMeasure(std::vector<tdcHit*> &v_Hits) : Measurement(v_Hits) {
+    if (fNmeasurements != 2) {
+        std::cout << "This is a double measurement, but number of hits in the measurement is " << fNmeasurements << std::endl;
+        std::cout << "Exiting..." << std::endl;
+    }
+
+    AnalyzeMeasurement();
+}
+
+void DoubleMeasure::AnalyzeMeasurement() {
+
+    int firstInd = f_v_Hits.at(0)->interval < f_v_Hits.at(1)->interval ? 0 : 1;
+    int secondInd = f_v_Hits.at(0)->interval < f_v_Hits.at(1)->interval ? 1 : 0;
+
+    f1stInterval = f_v_Hits.at(firstInd)->interval;
+    f1stTDCBin = f_v_Hits.at(firstInd)->tdcBin;
+    f1stRawTDC = f_v_Hits.at(firstInd)->rawTDC;
+
+    f2ndInterval = f_v_Hits.at(secondInd)->interval;
+    f2ndTDCBin = f_v_Hits.at(secondInd)->tdcBin;
+    f2ndRawTDC = f_v_Hits.at(secondInd)->rawTDC;
+}
+
+int DoubleMeasure::Get1stInterval() {
+    return f1stInterval;
+}
+
+int DoubleMeasure::Get1stTDCBin() {
+    return f1stTDCBin;
+}
+
+int DoubleMeasure::Get1stRawTDC() {
+    return f1stRawTDC;
+}
+
+int DoubleMeasure::Get2ndInterval() {
+    return f2ndInterval;
+}
+
+int DoubleMeasure::Get2ndTDCBin() {
+    return f2ndTDCBin;
+}
+
+int DoubleMeasure::Get2ndRawTDC() {
+    return f2ndRawTDC;
+}
+
+
+//____________________________________________________________________________
+
+TripleMeasure::TripleMeasure(std::vector<tdcHit*> &v_Hits) : Measurement(v_Hits) {
+    if (fNmeasurements != 3) {
+        std::cout << "This is a triple measurement, but number of hits in the measurement is " << fNmeasurements << std::endl;
+        std::cout << "Exiting..." << std::endl;
+    }
+
+    AnalyzeMeasurement();
+}
+
+void TripleMeasure::AnalyzeMeasurement() {
+    
+    sort( f_v_Hits.begin(), f_v_Hits.end(), vfTDCFuncs::CompareInterval );
+    
+    f1stInterval = f_v_Hits.at(0)->interval;
+    f1stTDCBin = f_v_Hits.at(0)->tdcBin;
+    f1stRawTDC = f_v_Hits.at(0)->rawTDC;
+    f2ndInterval = f_v_Hits.at(1)->interval;
+    f2ndTDCBin = f_v_Hits.at(1)->tdcBin;
+    f2ndRawTDC = f_v_Hits.at(1)->rawTDC;
+    f3rdInterval = f_v_Hits.at(2)->interval;
+    f3rdTDCBin = f_v_Hits.at(2)->tdcBin;
+    f3rdRawTDC = f_v_Hits.at(2)->rawTDC;
+}
+
+
+bool vfTDCFuncs::CompareInterval( tdcHit* h1, tdcHit* h2){
+  return (h1->interval < h2->interval);
+}
